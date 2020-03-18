51aa1b17a1c5ff1e7f055a0011c4051308ae97a8
Zane Castillo <zanecastillo3@gmail.com>
2020-03-18 12:33:01 -0400

    Create main.py
    Adding main.py to repository.

---
+++ main.py
@@ -0,0 +1,26 @@
+#Create variables with string values.
+s1 = "abc";
+s2 = "bcd";
+
+# Using the set method to assign values to new variables. 
+string1 = set(s1)
+string2 = set(s2)
+
+#Create variable to display common or mapped characters in two string variables by using the intersection method.
+s3 = string1.intersection(string2)
+
+#Prints out values that are mapped in both.
+#print(s3)
+
+#Determine if one string can map each character to another string.
+def s3():
+	if string1 != string2:
+		return True
+	else:
+		return False
+
+
+
+
+		
+
